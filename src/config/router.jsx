21013a1f061841bd89d7d60315823c75b96831f4
Vinh7252 <ngducvinh72@gmail.com>
2023-05-17 10:14:56 +0700

    update 2

--- src/config/router.jsx
+++ src/config/router.jsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Dropdown from "../components/Dropdown";
 
 const router = createBrowserRouter([
     {
         path: "/test",
-        element: <div>Hello world!</div>,
+        element: <Navbar/>,
+    },
+    {
+        path: "/test2",
+        element: <Dropdown/>,
     },
 ]);
 
